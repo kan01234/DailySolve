832ecb56297791bcc4a259f0a7316159555f17d5
kan <kan@bearbearkingdom>
2017-05-28 21:46:38 +0800

    [update] add fibonacciNumber

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+###[Fibonacci number](https://en.wikipedia.org/wiki/Fibonacci_number)
+
 ####[Text Justification](https://codefights.com/interview/ibANT8ZGc3shACBRF)
 
 ####[Sum of Two](https://codefights.com/interview/qAL6AiSejoJZRNyox)
---
+++ src/daily/solve/fibonacci/number/FibonacciNumber.java
@@ -0,0 +1,18 @@
+package daily.solve.fibonacci.number;
+
+import java.math.BigInteger;
+
+public class FibonacciNumber {
+
+	BigInteger calc(int num) {
+		if(num < 2) return BigInteger.valueOf(num);
+		BigInteger a = BigInteger.ONE, b = BigInteger.ONE, c;
+		for(int i = 2; i < num; i++) {
+			c = b;
+			b = b.add(a);
+			a = c;
+		}
+		return b;
+	}
+
+}
