9f177abb02282fb9d0c789d11cdc2a4d0e35e554
kan01234 <safghjkl@gmail.com>
2017-05-28 11:47:48 +0800

    [update] text justify
    Change-Id: I143b953d6a3114a56068c6d7640829526beb05dd
    Signed-off-by: kan01234 <safghjkl@gmail.com>

---
+++ src/daily/solve/main/justification/TextJustify.java
@@ -0,0 +1,52 @@
+package daily.solve.main.justification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextJustify {
+	public String[] justify(String[] words, int maxLength) {
+		if(words == null || words.length < 1 || maxLength <= 0) return null;
+		List<String> list = new ArrayList<String>();
+		int pointer = 0, start = 0, end = -1, currentLength = 0;
+		while (pointer < words.length) {
+			if ((currentLength + words[pointer].length() + (end - start + 1)) <= maxLength) {
+				currentLength += words[pointer].length();
+				end = pointer;
+				pointer++;
+			} else {
+				list.add(wordsToLine(words, start, end, currentLength, maxLength, false));
+				start = pointer;
+				end = start - 1;
+				currentLength = 0;
+			}
+		}
+		list.add(wordsToLine(words, start, end, currentLength, maxLength, true));
+		return list.toArray(new String[list.size()]);
+	}
+
+	String wordsToLine(String[] words, int start, int end, int currentLength, int maxLength, boolean isLast) {
+		String str = words[start];
+		int count = end - start + 1;
+		if (count == 1 || isLast) {
+			for (int i = start + 1; i <= end; i++) {
+				str = append(str, " ", 1);
+				str += words[i];
+			}
+			return append(str, " ", (maxLength - currentLength - count + 1));
+		} else {
+			int space = (maxLength - currentLength) / (count - 1), group = (maxLength - currentLength) % (count - 1);
+			for (int i = start + 1; i <= end; i++) {
+				str = append(str, " ", (i - start) <= group ? space + 1 : space);
+				str += words[i];
+			}
+		}
+		return str;
+	}
+
+	String append(String originalStr, String newStr, int times) {
+		for (int i = 0; i < times; i++)
+			originalStr += newStr;
+		return originalStr;
+	}
+
+}
---
+++ src/daily/solve/test/justification/TextJustifyTest.java
@@ -0,0 +1,19 @@
+package daily.solve.test.justification;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import daily.solve.main.justification.TextJustify;
+
+public class TextJustifyTest {
+
+	TextJustify textJustify = new TextJustify();
+
+	@Test
+	public void test() {
+		Assert.assertArrayEquals(new String[]{ "This    is    an", "example  of text", "justification.  " },
+				textJustify.justify(new String[]{ "This", "is", "an", "example", "of", "text", "justification." }, 16));
+		Assert.assertArrayEquals(null, textJustify.justify(new String[0], 16));
+		Assert.assertArrayEquals(null, textJustify.justify(new String[]{ "This", "is", "an", "example", "of", "text", "justification." }, 0));
+	}
+}
