85f5aed9f8797905f68448ba7a673d0b9e4692c0
kan01234 <safghjkl@gmail.com>
2017-05-29 15:23:28 +0800

    [update] add string reformatter

---
+++ src/daily/solve/string/reformatter/StringReformatter.java
@@ -0,0 +1,15 @@
+package daily.solve.string.reformatter;
+
+public class StringReformatter {
+
+	String stringReformatting(String s, int k) {
+	    s = new StringBuffer(s.replace("-", "")).reverse().toString();
+	    String s2 = "";
+	    for(int i = 0; i < s.length(); i ++) {
+	        s2 += s.charAt(i);
+	        if((i + 1) % k == 0 && (i + 1) != s.length()) s2 += "-";
+	    }
+	    return new StringBuffer(s2).reverse().toString();
+	}
+
+}
