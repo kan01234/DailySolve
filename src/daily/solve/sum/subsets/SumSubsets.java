cde1fcf050059c2a8ecf1688953f5eb52ce435f2
kan01234 <safghjkl@gmail.com>
2017-05-31 23:39:19 +0800

    [update] temp sum of subsets

--- src/daily/solve/sum/subsets/SumSubsets.java
+++ src/daily/solve/sum/subsets/SumSubsets.java
@@ -10,44 +10,50 @@ import java.util.stream.Collectors;
 public class SumSubsets {
 
 	List<List<Integer>> result;
-	Map<String, List<Integer>> map;
+	List<Integer> done;
 
 	List<List<Integer>> sumSubsets(int[] arr, int num) {
 		result = new ArrayList<List<Integer>>();
-		map = new HashMap<String, List<Integer>>();
-		if(arr.length == 0) {
+		done = new ArrayList<Integer>();
+		if (arr.length == 0) {
 			result.add(new ArrayList<Integer>());
 			return result;
 		}
+		// Arrays.sort(arr);
 		List<Integer> nums = new ArrayList<Integer>();
 		for (int i : arr)
 			nums.add(i);
-		Collections.sort(nums);
-		sumSubsets(nums, new ArrayList<Integer>(), num, true, "");
-		result = result.stream().distinct().collect((Collectors.toList()));
+		sumSubsets(nums, new ArrayList<Integer>(), num, true);
+//		result = result.stream().distinct().collect((Collectors.toList()));
 		System.out.println(result);
 		return result;
 	}
 
-	void sumSubsets(List<Integer> nums, List<Integer> last, int num, boolean isfirstLevel, String key) {
+	void sumSubsets(List<Integer> nums, List<Integer> used, int num, boolean isFirst) {
 		List<Integer> tmp, list;
 		int cur, diff;
-		for(int i = 0; i < nums.size(); i++) {
+		for (int i = 0; i < nums.size(); i++) {
 			cur = nums.get(i);
+			if (isFirst) {
+				if(done.indexOf(cur) >= 0) continue;
+				else done.add(cur);
+			} else {
+				if(nums.stream().min(Integer::compare).get() > cur) break;
+			}
+			list = new ArrayList<Integer>(used);
 			diff = num - cur;
-//			list.add(cur);
-			list = new ArrayList<Integer>(last);
-			tmp = new ArrayList<Integer>();
-			tmp.addAll(nums.subList(0, i));
-			tmp.addAll(nums.subList(i + 1, nums.size()));
-			if(isfirstLevel) key = cur + "";
+			list.add(cur);
 			if (diff == 0) {
-				System.out.println(list);
 				Collections.sort(list);
 				result.add(list);
-			} else if(diff > 0)
-				sumSubsets(tmp, list, diff, false, key);
+			} else if (diff > 0)
+				sumSubsets(removeFromList(nums, cur), list, diff, false);
 		}
 	}
 
+	List<Integer> removeFromList(List<Integer> list, Integer i) {
+		List<Integer> clone = new ArrayList<Integer>(list);
+		clone.remove(i);
+		return clone;
+	}
 }
