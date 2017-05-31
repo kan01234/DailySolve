cacc21bf83840f9f882b2ebd70cfba847accb2a4
kan01234 <safghjkl@gmail.com>
2017-05-31 17:44:58 +0800

    [update] sum subsets update

--- src/daily/solve/sum/subsets/SumSubsets.java
+++ src/daily/solve/sum/subsets/SumSubsets.java
@@ -1,53 +1,52 @@
 package daily.solve.sum.subsets;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SumSubsets {
 
-	Set<List<Integer>> set;
+	List<List<Integer>> result;
+	Map<String, List<Integer>> map;
 
 	List<List<Integer>> sumSubsets(int[] arr, int num) {
-		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		result = new ArrayList<List<Integer>>();
+		map = new HashMap<String, List<Integer>>();
 		if(arr.length == 0) {
 			result.add(new ArrayList<Integer>());
 			return result;
 		}
-		set = new LinkedHashSet<List<Integer>>();
-		Arrays.sort(arr);
 		List<Integer> nums = new ArrayList<Integer>();
 		for (int i : arr)
 			nums.add(i);
-		sumSubsets(nums, new ArrayList<Integer>(), num, true);
-		Iterator<List<Integer>> iter = set.iterator();
-		while(iter.hasNext()) {
-			result.add(iter.next());
-		}
+		Collections.sort(nums);
+		sumSubsets(nums, new ArrayList<Integer>(), num, true, "");
+		result = result.stream().distinct().collect((Collectors.toList()));
 		System.out.println(result);
 		return result;
 	}
 
-	void sumSubsets(List<Integer> nums, List<Integer> last, int num, boolean isFirst) {
+	void sumSubsets(List<Integer> nums, List<Integer> last, int num, boolean isfirstLevel, String key) {
 		List<Integer> tmp, list;
 		int cur, diff;
 		for(int i = 0; i < nums.size(); i++) {
+			cur = nums.get(i);
+			diff = num - cur;
+//			list.add(cur);
 			list = new ArrayList<Integer>(last);
 			tmp = new ArrayList<Integer>();
 			tmp.addAll(nums.subList(0, i));
 			tmp.addAll(nums.subList(i + 1, nums.size()));
-			cur = nums.get(i);
-			diff = num - cur;
-			list.add(cur);
+			if(isfirstLevel) key = cur + "";
 			if (diff == 0) {
+				System.out.println(list);
 				Collections.sort(list);
-				set.add(list);
+				result.add(list);
 			} else if(diff > 0)
-				sumSubsets(tmp, list, diff, false);
+				sumSubsets(tmp, list, diff, false, key);
 		}
 	}
 
